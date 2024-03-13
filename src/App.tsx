d3d777054b352d054726d06f99beb56b7cf6d40e
naka520 <yashipipi0520@gmail.com>
2024-03-14 02:29:29 +0900

    [add]ホーム画面とりあえず作った あとで幅調節と変数変更

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TopPage from "./components/TopPage";
-
+import GroupList from "./components/GroupList";
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<TopPage />} />
+        <Route path="/GroupList" element={<GroupList />} />
         {/* 以下のコメントアウトされたルートも、必要に応じて修正してください */}
         {/* <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} /> */}
---
+++ src/components/GroupList.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "bulma/css/bulma.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+interface GroupItem {
+  id: number;
+  name: string;
+  type: "managed" | "joined";
+}
+
+// モックデータ
+const mockData: GroupItem[] = [
+  { id: 1, name: "営業部会議", type: "managed" },
+  { id: 2, name: "総務全体会議", type: "joined" },
+];
+
+const GroupList: React.FC = () => {
+  // グループタイプに基づいてリストを分ける
+  const managedGroups = mockData.filter(group => group.type === "managed");
+  const joinedGroups = mockData.filter(group => group.type === "joined");
+
+  const handleAddGroup = () => {
+    // グループ追加処理
+    console.log("Add group clicked");
+  };
+
+  return (
+    <div className="container">
+      <section className="section">
+        <div className="box">
+          <div className="level">
+            <div className="level-left">
+              <h1 className="title">グループ一覧</h1>
+            </div>
+            <div className="level-right">
+              <button className="button is-primary" onClick={handleAddGroup}>
+                <FontAwesomeIcon icon={faPlus} />
+              </button>
+            </div>
+          </div>
+          <h2 className="title is-4">あなたが管理しているグループ</h2>
+          {managedGroups.map(group => (
+            <div
+              key={group.id}
+              className="box"
+              style={{ marginBottom: "10px" }}
+            >
+              {group.name}
+            </div>
+          ))}
+          <h2 className="title is-4" style={{ marginTop: "20px" }}>
+            あなたが参加しているグループ
+          </h2>
+          {joinedGroups.map(group => (
+            <div
+              key={group.id}
+              className="box"
+              style={{ marginBottom: "10px" }}
+            >
+              {group.name}
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default GroupList;
